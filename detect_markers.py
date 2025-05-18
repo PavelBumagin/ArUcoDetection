d3d2b56c23a0a269382f0d54c56781da8a6f16cf
user <pavel.bumagin@gmail.com>
2025-05-18 14:50:23 +0300

    angles fixed

--- detect_markers.py
+++ detect_markers.py
@@ -5,10 +5,9 @@ from scipy.spatial.transform import Rotation as R
 
 
 def rvec_to_euler_angles(rvec):
-    R_matrix, _ = cv2.Rodrigues(rvec)
-    rot = R.from_matrix(R_matrix)
-    euler = rot.as_euler('zyx', degrees=True)  # Yaw-Pitch-Roll (по X, Y, Z)
-    return euler  # в градусах
+    rot = R.from_rotvec(rvec)
+    euler = rot.as_euler('zyx', degrees=True) # Yaw-Pitch-Roll (по Z, Y, X)
+    return euler  # три угла в градусах
 
 
 # ==== Параметры камеры  ====
@@ -16,7 +15,7 @@ camera_matrix = np.array([[943.14025966, 0, 647.95301629],
                           [0, 907.76452814, 322.79856978],
                           [0,   0,   1]], dtype=np.float64)
 dist_coeffs = np.array([-0.1968091,   0.54916959,  0.01793236,  0.00326459, -0.48967739])
-# ==== Настройки ArUco ====
+# ==== Выбор одного из существующих наборов маркеров ====
 aruco_dict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
 
 
@@ -42,12 +41,12 @@ while True:
             cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvecs[i], tvecs[i], 0.03, thickness=2)
             angles = rvec_to_euler_angles(rvecs[i][0])
             # Выводим в консоль позицию и ориентацию
-            print(f"ID: {ids[i][0]} | Pos: {tvecs[i][0]} | Angles: {angles}")
+            print(f"ID: {ids[i][0]} | Позиция: {tvecs[i][0]} | Углы поворота: {angles}")
 
     #выход по нажатию клавиши q
     cv2.imshow('Pose Estimation', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
+#Прекращение захвата изображения, закрытие окна
 cap.release()
 cv2.destroyAllWindows()
